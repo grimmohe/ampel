c8fd3f9fe2f7b66659297dba13fa47cb343cdf99
grimm <chis.o.henning@gmail.com>
2018-02-20 20:38:19 +0100

    add manual garbage collect

--- start_with_net.py
+++ start_with_net.py
@@ -4,7 +4,7 @@ from learner import Learner
 
 logging.basicConfig(level=logging.INFO)
 
-learner = Learner(20, 4, 4, 0.2)
+learner = Learner(genomeUnits=20, selection=5, mutations=5, mutationProb=0.2)
 learner.state = 'LEARNING'
 learner.startLearning()
 
