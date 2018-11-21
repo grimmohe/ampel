e4627553956e38c7ff7c5ee41fb1d9ef5a07edc9
grimm <chis.o.henning@gmail.com>
2018-11-21 22:33:23 +0100

    sim architecture

---
+++ src/sim/simulator.py
@@ -0,0 +1,66 @@
+import time
+import sys
+from sim.model import Car, Street, Model
+
+"""
+"""
+class Simulator(object):
+
+    def __init__(self, model = Model()):
+        self.model = model
+        self.time = time.time()
+
+    """
+    1. Signal vom Verkehrsnetz
+    2. Reaktion vom Tensor
+
+    Jeder Reaktion des Verkehrs mit Ergebnis vom Tensor folgt ein weiteres Signal.
+    Nach einer Sekunde ohne Events im Verkehr folgt ein weiteres Signal an den Tensor.
+
+    step() springt an den nächsten Zeitpunkt der ein Signal vom Verkehrsnetz erfordert.
+    """
+    def step(self, callbackMethod):
+        event = self._getNextEvent()
+
+        if event.distance > 1:
+            event.carId = None
+            event.destinantionId = None
+            event.distance = 1.
+            event.streetId = None
+
+        self._moveCars(event.distance)
+
+        timestamp = time.time()
+
+        action = callbackMethod(event)
+
+        self._moveCars(time.time() - timestamp)
+        self._applyAction(action)
+
+
+    def _getNextEvent(self):
+        next = _Event()
+
+        for car in self.model.cars:
+            if car.distance < next.distance:
+                next.carId = car.carId
+                next.streetId = car.streetId
+                next.destinantionId = car.destinantionId
+                next.distance = car.distance
+
+        return next
+
+    def _moveCars(self, distance=.0):
+        self.model.cars.sort(key=lambda x: x.distance)
+
+    def _applyAction(self, action):
+        pass
+
+
+class _Event(object):
+
+    def __init__(self):
+        self.distance = sys.float_info.max
+        self.carId = 0
+        self.streetId = 0
+        self.destinantionId = 0
