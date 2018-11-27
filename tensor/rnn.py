0426338642e148b69b3593c3d9cc915fc2d43807
carcinoma <carcinoma@gmx.net>
2018-11-27 22:04:49 +0100

    cleanup

--- tensor/rnn.py
+++ tensor/rnn.py
@@ -16,11 +16,8 @@ class RNN(object):
     def init_rnn(self):
 
         self.x = tf.placeholder('float', [None, self.n_chunks, self.chunk_size])
-        #y = tf.placeholder('float')
 
         self.prediction = self.recurrent_neural_network(self.x)
-        #self.cost = tf.reduce_mean( tf.nn.softmax_cross_entropy_with_logits_v2(logits=self.prediction,labels=y) )
-        #self.optimizer = tf.train.AdamOptimizer().minimize(self.cost)
         
         config = tf.ConfigProto(
             device_count = {'GPU': 0}
@@ -36,14 +33,8 @@ class RNN(object):
         x = tf.transpose(x, [1,0,2])
         x = tf.reshape(x, [-1, self.chunk_size])
         x = tf.split(x, self.n_chunks, 0)
-        #x = tf.shape(x)
-        #x = tf.unstack(x, num=1)
-        #x = tf.TensorArray(tf.int32, 1, dynamic_size=True, infer_shape=False)
-        #x = TensorArr.unpack(x)
 
         lstm_cell = rnn_cell.LSTMCell(self.rnn_size,state_is_tuple=True)
-        #lstm_cell2 = rnn_cell.LSTMCell(rnn_size,state_is_tuple=True)
-        #outputs = rnn.static_rnn(lstm_cell, x, dtype=tf.float32)
         outputs, states=rnn.static_rnn(lstm_cell, x, dtype=tf.float32)
 
         output = tf.matmul(outputs[-1],layer['weights']) + layer['biases']
@@ -52,7 +43,6 @@ class RNN(object):
 
 
     def train_neural_network(self, event=Event()):
-        print(type(self.x))
         _, c = self.sess.run(self.prediction, feed_dict={self.x: [
             [
                 [event.distance, event.carId, event.streetId, event.destinantionId]
@@ -62,8 +52,3 @@ class RNN(object):
             ]
         ]})
         return Action(_[0], _[1])
-        #epoch_loss += c
-
-        #correct = tf.equal(tf.argmax(prediction, 1), tf.argmax(y, 1))
-        #accuracy = tf.reduce_mean(tf.cast(correct, 'float'))
-        #print('Accuracy:',accuracy.eval({x:mnist.test.images.reshape((-1, n_chunks, chunk_size)), y:mnist.test.labels}))
